7eaab821296e38da23f3c33d99bd577c4ae4edd1
YuliaKravchukS <ljuljek554@gmail.com>
2024-03-26 12:24:57 +0200

    goood

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ let timedEvent;
 let remainingTime;
 let coinMusic;
 let bgMusic;
-let money;
 let emitter;
 let points = 0;
 const speedDown = 500;
@@ -18,7 +17,7 @@ const playerSpeed = speedDown + 50;
 
 const sizes={
   width:500,
-  height:500
+  height:450
 }
 
 const refs={
@@ -27,8 +26,12 @@ const refs={
   gameEndDiv:document.querySelector('#gameEndDiv'),
   gameWinLoseSpan:document.querySelector('#gameWinLoseSpan'),
   gameEndScoreSpan:document.querySelector('#gameEndScoreSpan'),
+  gameRestartBtn:document.querySelector('#gameRestartBtn'),
+  gameSpam:document.querySelector('#gameSpam'),
+  link:document.querySelector('#link'),
+  gameResult:document.querySelector('#gameResult')
 }
-
+console.log(refs.link);
 const config={
   type: Phaser.WEBGL,
   width:sizes.width,
@@ -49,25 +52,18 @@ const config={
 }
 
 function getRandomX() {
-  return Math.floor(Math.random()*600);
+  return Math.floor(Math.random()*400);
 }
-  function getHit() {
-    coinMusic.play();
-    target.setY(0);
-    target.setX(getRandomX());
-    points++;
-    textScore.setText(`Score:${points}`)
-    emitter.start()
-  }
+
   const game = new Phaser.Game(config);
 
  
 
 function  preload(){
-    this.load.image('bgf', 'assets/bgf.jpg');
+    this.load.image('bgt', 'assets/bgt.png');
     this.load.image('jar', 'assets/jar.png');
     this.load.image('bit1', 'assets/bit1.png');
-    this.load.image('moneyb', 'assets/moneyb.png');
+    this.load.image('moneyd', 'assets/moneyd.png');
     this.load.audio('bgMusic', 'assets/bgMusic.mp3');
     this.load.audio('coin', 'assets/coin.mp3');
    
@@ -79,10 +75,10 @@ function  preload(){
     coinMusic=this.sound.add('coin');
     bgMusic=this.sound.add('bgMusic');
     bgMusic.play();
-     bgMusic.stop();
+    //  bgMusic.stop();
       
-    this.add.image(0, 0,'bgf').setOrigin(0, 0);    
-    player = this.physics.add.image(0, sizes.height-100,'jar')
+    this.add.image(0, 0,'bgt').setOrigin(0, 0);    
+    player = this.physics.add.image(0, sizes.height,'jar')
     player.setImmovable(true)
     player.body.allowGravity = false
     player.setCollideWorldBounds(true)
@@ -96,24 +92,40 @@ function  preload(){
     textScore = this.add.text(sizes.width-110, 16, 'Score: 0', { fontSize: '22px Arial', fill: '#000' });
     textTime = this.add.text(16, 16, 'Remaining Time: 00', { fontSize: '22px Arial', fill: '#000' });
     timedEvent= this.time.delayedCall(30000,gameOver,[],this)
-    emitter= this.add.particles(0,0,'moneyb',{
-      speed:100,
-      gravityY:speedDown-180,
-      scale:0.04,
+    
+    emitter= this.add.particles(0,0,'moneyd',{
+      speed:80,
+      gravityY:speedDown-200,
+      scale:0.05,
       duration:100,
       emitting:false,
     });
-    emitter.startFollow(player,player.width/6, player.height/6,true)
+    emitter.startFollow(player,player.width/10, player.height/20,true)
+
+    function getHit() {
+      coinMusic.play();
+      target.setY(0);
+      target.setX(getRandomX());
+      points++;
+      textScore.setText(`Score:${points}`)
+      emitter.start()
+    }
+
     function gameOver() {
       this.sys.game.destroy(true);
       if(points>=30){
         refs.gameEndScoreSpan.textContent= points
-        refs.gameWinLoseSpan.textContent= 'Win'
+        refs.gameWinLoseSpan.textContent= 'Victory🥳!'
+        refs.gameSpam.textContent= 'Please help Ukraine win! Donate👇'
+        refs.gameResult.classList.add('resultWin')
       }else{
         refs.gameEndScoreSpan.textContent= points
-        refs.gameWinLoseSpan.textContent= 'Lost!🙄 Don\'t forget to donate👇'
+        refs.gameWinLoseSpan.textContent= 'Lost!🙄'
+        refs.gameSpam.textContent= 'Don\'t forget to donate👇'
+        refs.gameResult.classList.add('result')
       }
       refs.gameEndDiv.style.display ='flex'
+      refs.link.style.display ='block'
     }
   
   }
@@ -142,3 +154,10 @@ refs.gameStartBtn.addEventListener('click',()=>{
   game.scene.resume('scene-game');
 })
 
+refs.gameRestartBtn.addEventListener('click',()=>{
+  refs.gameStartDiv.style.display='none';
+  refs.gameEndDiv.style.display='none';
+  game.destroy();
+  points = 0;
+  const newGame = new Phaser.Game(config);
+ })
